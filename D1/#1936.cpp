52c9c7befc26d9ba58b9f5a761e585bae211981e
Minseong Kim <emeraldgoose@gmail.com>
2019-09-06 23:22:49 +0900

    no message

---
+++ D1/#1936.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin>>a>>b;
+    if(a==1) {
+        if(b==3) cout<<"A";
+        else if(b==2) cout<<"B";
+    }
+    else if (a==2) {
+        if(b==1) cout<<"A";
+        else if(b==3) cout<<"B";
+    }
+    else {
+        if(b==1) cout<<"B";
+        else if(b==2) cout<<"A";
+    }
+    cout<<'\n';
+    return 0;
+}
