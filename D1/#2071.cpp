66c665a85ff9d25de9f336a8a144e99bad310f82
emeraldgoose <50171632+emeraldgoose@users.noreply.github.com>
2019-09-07 11:59:42 +0900

    Update #2071.cpp

--- D1/#2071.cpp
+++ D1/#2071.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 int main() {
  	int testCase, tc;
-    cin>>testCase;
-    for(tc=1;tc<=testCase;tc++) {
-        int seq[10], total=0;
-        for(int i=0;i<10;i++) {
-            cin>>seq[i]; total += seq[i];
-    	}
-        cout<<"#"<<tc<<" "<<(total+5)/10<<endl;
-    }
-    return 0;
+  cin>>testCase;
+  for(tc=1;tc<=testCase;tc++) {
+      int seq[10], total=0
+      for(int i=0;i<10;i++) {
+          cin>>seq[i]; total += seq[i];
+    	 }
+      cout<<"#"<<tc<<" "<<(total+5)/10<<endl;
+  }
+  return 0;
 }
