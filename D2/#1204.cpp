c8cb8956f282cf3a3a71a257a3e31dc41fbac7ec
emeraldgoose <50171632+emeraldgoose@users.noreply.github.com>
2019-09-07 18:34:23 +0900

    Update #1204.cpp

--- D2/#1204.cpp
+++ D2/#1204.cpp
@@ -3,23 +3,23 @@
 using namespace std;
 int main() {
  	int testCase, a, testnum;
-    cin>>testCase;
+  cin>>testCase;
  	for(a=1;a<=testCase;a++) {
-        int i, num;
-        int cnt[101], max=0, res;
-        cin>>testnum;
-        for(i=0;i<101;i++)
-            cnt[i] = 0;
-         for(i=0;i<1000;i++) {
-        	cin>>num;
-            cnt[num] += 1;
-        }
-        for(i=0;i<101;i++) {
-            if(cnt[i] >= max) {
-                max=cnt[i]; res = i;
-            }
-        }
-        cout<<"#"<<testnum<<" "<<res<<endl;
-    }
-    return 0;
+      int i, num;
+      int cnt[101], max=0, res;
+      cin>>testnum;
+      for(i=0;i<101;i++)
+          cnt[i] = 0;
+      for(i=0;i<1000;i++) {
+         	cin>>num;
+          cnt[num] += 1;
+      }
+      for(i=0;i<101;i++) {
+          if(cnt[i] >= max) {
+              max=cnt[i]; res = i;
+          }
+      }
+      cout<<"#"<<testnum<<" "<<res<<endl;
+  }
+  return 0;
 }
