00b62373614ea2c00ccaf9d89f94f5f733a7781b
emeraldgoose <50171632+emeraldgoose@users.noreply.github.com>
2019-09-07 12:10:20 +0900

    Update #2058.cpp

--- D1/#2058.cpp
+++ D1/#2058.cpp
@@ -1,7 +1,7 @@
 // 2058. 자릿수 더하기
 #include <cstdio>
 int main() {
- 	int num[4]={0}, sum=0;
+    int num[4]={0}, sum=0;
     for(int i=0;i<4;i++) {
         scanf("%1d",&num[i]); sum+=num[i];
     }
