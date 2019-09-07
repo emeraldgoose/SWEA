2e75017edcd3969bc9fb941638dd01b3f6a27608
emeraldgoose <50171632+emeraldgoose@users.noreply.github.com>
2019-09-07 11:58:47 +0900

    Update #2072.cpp

--- D1/#2072.cpp
+++ D1/#2072.cpp
@@ -2,13 +2,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int testCase, tc;
+    int testCase, tc;
     cin>>testCase;
     for(tc=1;tc<=testCase;tc++) {
         int total = 0,seq[10];
     	for (int i=0;i<10;i++) {
             cin>>seq[i];
-        	if (seq[i]%2 == 1) total += seq[i];
+       	    if (seq[i]%2 == 1) total += seq[i];
         }
         cout<<"#"<<tc<<" "<<total<<endl;
     }
