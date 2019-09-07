f3b0488cee9111614d4166c4ffabca333edaff2c
emeraldgoose <50171632+emeraldgoose@users.noreply.github.com>
2019-09-07 12:01:10 +0900

    Update #2068.cpp

--- D1/#2068.cpp
+++ D1/#2068.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 int main() {
  	int testCase, tc;
-    cin>>testCase;
-    for(tc=1;tc<=testCase;tc++) {
-        int seq[10], max=0;
-     	for(int i=0;i<10;i++) {
-            cin>>seq[i];
-         	if(seq[i]>max) max = seq[i];
-        }
-        cout<<"#"<<tc<<" "<<max<<endl;
-    }  
-    return 0;
+  cin>>testCase;
+  for(tc=1;tc<=testCase;tc++) {
+    int seq[10], max=0;
+    for(int i=0;i<10;i++) {
+        cin>>seq[i];
+       	if(seq[i]>max) max = seq[i];
+    }
+    cout<<"#"<<tc<<" "<<max<<endl;
+  }  
+  return 0;
 }
