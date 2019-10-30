990d54a3ba62123e4f64a6f78045dbdb0282b0ca
emeraldgoose <50171632+emeraldgoose@users.noreply.github.com>
2019-10-30 14:10:51 +0900

    Update #8821.cpp

--- D3/#8821.cpp
+++ D3/#8821.cpp
@@ -8,7 +8,7 @@ int main() {
     for(int a=1;a<=testCase;a++) {
         int ans[10]; memset(ans,0,sizeof(int)*10);
         string str;
-		cin>>str;
+        cin>>str;
         for(int i=0;i<str.length();i++) ans[(str[i]-'0')]++;
         int cnt=0;
         for(int i=0;i<10;i++) {
