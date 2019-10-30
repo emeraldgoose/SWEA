b5388c5f7d1e2bf9efd37a71d12df9bbcebe289f
Minseong Kim <emeraldgoose@gmail.com>
2019-10-30 14:10:03 +0900

    no message

---
+++ D3/#8821.cpp
@@ -0,0 +1,21 @@
+// 8821. 적고 지우기
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main() {
+    int testCase;
+    cin>>testCase;
+    for(int a=1;a<=testCase;a++) {
+        int ans[10]; memset(ans,0,sizeof(int)*10);
+        string str;
+		cin>>str;
+        for(int i=0;i<str.length();i++) ans[(str[i]-'0')]++;
+        int cnt=0;
+        for(int i=0;i<10;i++) {
+            ans[i]%=2;
+            if(ans[i]==1) cnt++;
+        }
+        cout<<"#"<<a<<" "<<cnt<<endl;
+    }
+    return 0;
+}
